ba7d24b76558a2b864c84ae5b1c95638a679c909
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-03-11 13:17:44 +0300

    Yapılar düzenlendi, Countries, Footer  eklendi

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Grid } from '@mui/material';
 import SliderImage from "../../../assets/slider.png"
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import "./Slider.scss"
 
 const Slider = () => {
     return (
         <div className='grid-container'>
             <Container maxWidth="lg">
-                <Grid container spacing={3}>
+                <Grid container>
                     <Grid item xs={12} style={{ position: 'relative' }}>
                         <div className="image-container">
                             <img src={SliderImage} alt="Slider Resmi" style={{ width: '100%', maxHeight: "250px", objectFit: "cover", borderRadius: "30px", }} />
@@ -30,45 +31,3 @@ export default Slider;
 
 
 
-
-
-//  <Container maxWidth="lg">
-//             <Typography variant='h5' align='center' style={{ marginTop: "50px" }}>
-//                 Material UI Responsive
-//             </Typography>
-//             <Grid>
-//                 {Data.map((result, index) => (
-//                     <Grid item xs={12} ms={4} key={index}>
-//                         <Card sx={{ maxWidth: 345 }}>
-//                             <CardActionArea>
-//                                 <CardMedia
-//                                     component="img"
-//                                     height="140"
-//                                     image={result.img}
-//                                     alt={result.title}
-//                                 />
-//                                 <CardContent> {/* CardContent bileşeni eklendi */}
-//                                 <Typography gutterBottom variant="h5" component="div">
-//                                 {result.title}
-//                             </Typography>
-//                             <Typography variant="body2" color="text.secondary">
-//                                 {result.des}
-//                             </Typography>
-//                         </CardContent>
-//                     </CardActionArea>
-//                 </Card>
-//             </Grid >
-//         ))}
-//     </Grid>
-// </Container>
-
-
-{/* <div className='slider'>
-<div className="background-container">
-    <img src="../assets/Slider.png" alt="Model1" />
-    <div className='text-overlay'>
-        <h1>En Erken Vize Tarihini Bul</h1>
-        <p>Arayın ve anında bildirim alın!</p>
-    </div>
-</div>
-</div> */}
